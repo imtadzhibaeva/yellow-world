2ab82f8369d0c4e2d0197f57ac914ccbf056619a
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2024-06-05 14:02:57 -0400

    react component

---
+++ src/YellowWorld.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const YellowWorld = () => {
+  const style = {
+    backgroundColor: 'yellow',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '2rem',
+  };
+
+  return (
+    <div style={style}>
+      Yellow World
+    </div>
+  );
+};
+
+export default YellowWorld;
